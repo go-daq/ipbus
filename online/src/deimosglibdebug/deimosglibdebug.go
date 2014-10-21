9f90af6762153c953a84d80941e8dca3df0b2590
Nick Ryder <nick.ryder@physics.ox.ac.uk>
2014-10-21 17:17:08 +0100

    Starting to use sm1daq code to debug DEIMOS and GLIB.

---
+++ online/src/deimosglibdebug/deimosglibdebug.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "data"
+    "mail"
+    "flag"
+    "fmt"
+    "solid"
+    "net"
+    "runtime"
+    "time"
+)
+
+func cleanexit(e mail.E) {
+    if r := recover(); r != nil {
+        if err, ok := r.(error); ok {
+            ep := data.MakeErrPack(err)
+            fmt.Printf("Caught a panic: %v\n", ep)
+            subject := fmt.Sprintf("Online DAQ crash at %v", time.Now())
+            msg := fmt.Sprintf("Caught a panic: %v\n", ep)
+            if err := e.Send(subject, msg); err != nil {
+                fmt.Println(err)
+            }
+        } else {
+            panic(r)
+        }
+    }
+    fmt.Println("Clean exit.")
+}
+
+
+func main() {
+    dir := flag.String("dir", ".", "output directory")
+    period := flag.Int("time", 10, "Length of run [s]")
+    allowmod := flag.Bool("allowmod", false, "Allow running even if code modified.")
+    passfile := flag.String("pass", "pass.txt", "Email password file.")
+    flag.Parse()
+    e := mail.E{}
+    err := e.Load(*passfile)
+    if err != nil {
+        panic(err)
+    }
+    defer cleanexit(e)
+    runtime.GOMAXPROCS(4)
+    fmt.Println("Solid's SM1 online DAQ software!")
+    control := solid.New(*dir)
+    addr, err := net.ResolveUDPAddr("192.168.200.3:50001")
+    if err != nil {
+        panic(err)
+    }
+    control.AddFPGA(addr)
+    errp := control.Start()
+    if errp.Err != nil {
+        fmt.Printf("Error in Start(): %v\n", errp)
+        e.Log("Error in start:", errp)
+        return
+    }
+    dt := time.Duration(*period) * time.Second
+    r, err := data.NewRun(0, "test", dt)
+    if err != nil {
+        panic(err)
+    }
+    if r.Commit.Modified && !(*allowmod) {
+        panic(fmt.Errorf("Code has local modifications: %v\n", r.Commit))
+    }
+    errp = control.Run(r)
+    if errp.Err != nil {
+        fmt.Printf("Error in run: %v\n", errp)
+        e.Log("Error in run:", errp)
+    }
+    stop := time.Now()
+    fmt.Printf("Stopped run at %v [%v]\n", stop, stop.Sub(r.Start))
+    control.Quit()
+}
