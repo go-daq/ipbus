b21c6c5fb5c289fdf62a8131d0b68b2883b27305
Nick Ryder <nick.ryder@physics.ox.ac.uk>
2015-02-15 11:15:25 +0000

    Script to handle running with crashes.

---
+++ online/src/run/crashhack.py
@@ -0,0 +1,20 @@
+import os
+
+tmpdir = "/data/fast/runatbr2/run/"
+storage = "/data/fast/runatbr2/stoarge/"
+
+runcmd ="../../bin/run -allowmod -glib \"GLIB1,GLIB2,GLIB3,GLIB4\" -threshold 150 -duration 300 -nrun -1 -coincidence -dir %s -store %s" % (tmpdir, storage)
+while True:
+    os.system(runcmd)
+    files = os.listidr(tmpdir)
+    for fn in files:
+        tmpfn = os.path.join(tmpdir, fn)
+        stat = os.stat(tmpfn)
+        size = stat.st_size * 1e-6 # MB
+        if size > 20:
+            storefn = os.path.join(storage, fn)
+            cmd = "mv %s %s" % (tmpfn, storefn)
+            os.system(cmd)
+        else:
+            cmd = "rm %s" % tmpfn
+            os.system(cmd)
