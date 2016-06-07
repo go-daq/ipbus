75be787b486847148309bdbe2805cc40a888e7cd
Nick Ryder <nick.ryder@physics.ox.ac.uk>
2016-06-07 17:45:10 +0100

    Starting to write package tests (see issue #7).

---
+++ ipbus/dummy.go
@@ -0,0 +1,48 @@
+package ipbus
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func newdummy(port int) dummyhardware {
+	/*
+	cmdstring := "DummyHardwareUdp.exe"
+	args := []string{"--version", "w", "--port"}
+	args = append(args, fmt.Sprintf("%d", port))
+	cmd := exec.Command(cmdstring, args)
+	*/
+	cmd := exec.Command("DummyHardwareUdp.exe", "--version", "2",
+						"--port", fmt.Sprintf("%d", port))
+	dummy := dummyhardware{cmd, false}
+	return dummy
+}
+
+// dummyhardware runs the IPbus dummy hardware program written in C++.
+// It should only be used for testing this package, which is why it is
+// not exported. It requires a local installation of the uhal package,
+// which itself requires running SLC6 or similar. This is the only
+// part of the ipbus package with that requirement.
+type dummyhardware struct {
+	cmd *exec.Cmd
+	running bool
+}
+
+
+func (d * dummyhardware) Start() error {
+	err := error(nil)
+	if !d.running {
+		err = d.cmd.Start()
+		d.running = true
+	}
+	return err
+}
+
+func (d * dummyhardware) Stop() error {
+	err := error(nil)
+	if d.running {
+		err = d.cmd.Process.Kill()
+		d.running = false
+	}
+	return err
+}
---
+++ ipbus/ipbus_test.go
@@ -0,0 +1,108 @@
+package ipbus
+
+import (
+	"math/rand"
+	"testing"
+)
+
+const failunwritten = true 
+
+// Ensure that creating a new target times out when there's no target present.
+func TestTimeout(t *testing.T) {
+
+	if failunwritten {
+		t.Errorf("Test function not yet implemented.")
+	}
+}
+
+// Test single word read and write.
+func TestSingleReadWrite(t *testing.T) {
+	/*
+	dummy := newdummy(50002)
+	if err := dummy.Start(); err != nil {
+		t.Error(err)
+	}
+
+	if err := dummy.Stop(); err != nil {
+		t.Error(err)
+	}
+	*/
+	if failunwritten {
+		t.Errorf("Test function not yet implemented.")
+	}
+}
+
+// Test block read and block write.
+func TestBlockReadWrite(t *testing.T) {
+
+	if failunwritten {
+		t.Errorf("Test function not yet implemented.")
+	}
+}
+
+// Test that the library returns correct errors when going against target's permissions.
+func TestPermissions(t *testing.T) {
+
+	if failunwritten {
+		t.Errorf("Test function not yet implemented.")
+	}
+}
+
+// Bench mark single word read.
+func BenchmarkSingleRead(b *testing.B) {
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+
+	}
+
+}
+
+
+// Bench mark single word write.
+func BenchmarkSingleWrite(b *testing.B) {
+	/*
+	target := Target{}
+	reg := Register{}
+	nvals := 1024
+	data := make([]uint32, nvals)
+	for i := 0; i < nvals; i++ {
+		data[i] = uint32(rand.Int31())
+	}
+	datum := make([]uint32, 1)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		datum[0] = data[n % nvals]
+		resp := target.Write(reg, datum)
+		target.Dispatch()
+		rep := <-resp
+		if rep.Err != nil {
+			b.Log(rep.Err)
+		}
+	}
+	*/
+
+}
+
+
+// Bench mark multi-packet block reads.
+func BenchmarkBlockRead(b *testing.B) {
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+
+	}
+
+}
+
+
+// Bench mark multi-packet block writes.
+func BenchmarkBlockWrite(b *testing.B) {
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+
+	}
+
+}
+
